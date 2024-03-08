470591d485a3fa6bb041c6fdfb4a56f2e8164e90
Yayat py <yayatpy@gmail.com>
2024-03-09 07:35:05 +0800

    #

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -6,8 +6,8 @@ import "./index.css";
 import { ToastContainer } from "react-toastify";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+  <>
     <App />
     <ToastContainer position="bottom-right" autoClose={1500} />
-  </React.StrictMode>
+  </>
 );
